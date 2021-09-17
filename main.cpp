9483446f4e122c5d128473282881e2d6c0cfc3cb
Vicente Viera <plantedcoot706@gmail.com>
2021-09-17 13:33:26 -0500

    agrego el git ignore, el main, agrego las librerias base y hago una funcion para pasar de meses a numeros

---
+++ main.cpp
@@ -0,0 +1,44 @@
+/*===============================*
+<Autores>
++Vicente Viera Guízar, ID: A01639784
++
++
+</Autores>
+=================================*
+*/
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <map>
+
+using namespace std;
+
+//funcion para pasar de meses a numeros
+
+//genero el map con los meses y su valor
+map<string,string> Map {
+    {"Jan","1"},
+    {"Feb","2"},
+    {"Mar","3"},
+    {"Apr","4"},
+    {"May","5"},
+    {"Jun","6"},
+    {"Jul","7"},
+    {"Aug","8"},
+    {"Sep","9"},
+    {"Oct","10"},
+    {"Nov","11"},
+    {"Dec","12"},
+};
+
+string mon2num(string month){
+    //regreso el valor numerico del map
+    return Map.find(month)->second;
+}
+
+int main(){
+    cout<<mon2num("Dec");
+
+    return 0;
+}
